56c8104cebb0b06bac22df1f740513713b3e303b
helt1nh0 <131688858+helt1nh0@users.noreply.github.com>
2024-10-15 08:37:38 -0300

    aaaaaaaaaaaaaaaaa

--- projeto2.js
+++ projeto2.js
@@ -18,12 +18,12 @@ function criaCartao(categoria,pergunta,resposta){
 }
 
 function showAnswer(card){
-    const answer= document.querySelector('.cartao-resposta');
+    const answer= card.querySelector('.cartao-resposta');
     if (answer.style.display === 'none'){
-        answer.style.display === 'block';
+        answer.style.display = 'block';
     }
     else{
-        answer.style.display === 'none';
+        answer.style.display = 'none';
     }
 
 }
