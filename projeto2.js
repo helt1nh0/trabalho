e3127c92f974b37ad85b0e8d020d74e72fa2fc65
helt1nh0 <131688858+helt1nh0@users.noreply.github.com>
2024-10-15 08:29:12 -0300

    aaaaaaaaaa

---
+++ projeto2.js
@@ -0,0 +1,29 @@
+function criaCartao(categoria,pergunta,resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `<div class="cartao-conteudo" onclick= "showAnswer(this)">
+    <h3> ${categoria} </h3>
+    
+    <div class="cartao-pergunta">
+        <p>${pergunta}</p>
+        </div>
+
+        <div class="cartao-resposta">
+        <p>${resposta}</p>
+        </div>
+        </div>
+        `
+       container.appendChild(cartao);
+}
+
+function showAnswer(card){
+    const answer= document.querySelector('.cartao-resposta');
+    if (answer.style.display === 'none'){
+        answer.style.display === 'block';
+    }
+    else{
+        answer.style.display === 'none';
+    }
+
+}
